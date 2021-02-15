3503c092a447bcd1cf7315bd2da3c7d3f1e0f153
kyunghoonkim <kkh3@cau.ac.kr>
2021-02-15 14:57:44 +0900

    ✨ Opacity를 사용하여 카드가 뒤집어졌을 때 어떤 카드인지 확인할 수 있도록 구현

--- Memorize/Cardify.swift
+++ Memorize/Cardify.swift
@@ -28,14 +28,15 @@ struct Cardify: AnimatableModifier {
     
     func body(content: Content) -> some View {
         ZStack {
-        if isFaceUp{
-            RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
-            RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
-            content
-        }
-        else {
-                RoundedRectangle(cornerRadius: cornerRadius).fill()
+            Group{
+                RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
+                RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
+                content
             }
+                .opacity(isFaceUp ? 1: 0)
+            RoundedRectangle(cornerRadius: cornerRadius).fill()
+                .opacity(isFaceUp ? 0 : 1)
+            
         }
         .rotation3DEffect(Angle.degrees(rotation), axis: (0, 1, 0))
     }
